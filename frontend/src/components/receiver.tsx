f09ab9deb4feac233abf0f7c290021afb21ad15c
10-abhi <abhishek81936@gmail.com>
2025-08-13 13:47:26 +0530

    added ice cand code

--- frontend/src/components/receiver.tsx
+++ frontend/src/components/receiver.tsx
@@ -1,25 +1,36 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-export const Receiver = ()=>{
-    useEffect(()=>{
-     const socket = new WebSocket('ws://localhost:8080');
-     socket.onopen = ()=>{
-        socket.send(JSON.stringify({type : "receiver"}));
-     }
-     socket.onmessage = async (event)=>{
-      console.log("Received msg", event.data);
-        const message = JSON.parse(event.data);
-        if(message.type === "create-offer"){
-           console.log("Matched create-offer");
-           const pc = new RTCPeerConnection();
-           pc.setRemoteDescription(message.sdp);
-           const answer =await pc.createAnswer();
-           pc.setLocalDescription(answer);
-           socket.send(JSON.stringify({type : "create-answer" , sdp : answer}));
-        }
-     }
-    }, [])
-    return <div>
+export const Receiver = () => {
+   const [pc, setPc] = useState<RTCPeerConnection | null>(null);
+   useEffect(() => {
+      const socket = new WebSocket('ws://localhost:8080');
+      socket.onopen = () => {
+         socket.send(JSON.stringify({ type: "receiver" }));
+      }
+      socket.onmessage = async (event) => {
+         console.log("Received msg", event.data);
+         const message = JSON.parse(event.data);
+         if (message.type === "create-offer") {
+            console.log("Matched create-offer");
+            const newpc = new RTCPeerConnection();
+            setPc(newpc);
+            console.log("the peer conn", pc);
+            await pc?.setRemoteDescription(message.sdp);
+            newpc.onicecandidate = (event) => {
+               console.log("icecand event", event);
+               if (event) {
+                  socket.send(JSON.stringify({ type: 'iceCandidate', candidate: event.candidate }));
+               }
+            }
+            const answer = await pc?.createAnswer();
+            await pc?.setLocalDescription(answer);
+            socket.send(JSON.stringify({ type: "create-answer", sdp: answer }));
+         } else if (message.type === "iceCandidate") {
+            await pc?.addIceCandidate(message.candidate);
+         }
+      }
+   }, [])
+   return <div>
       Receiver
-    </div>
+   </div>
 }
