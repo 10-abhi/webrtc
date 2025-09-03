b9e675663c2f1edf7db9d5414e8aa952afa9f45f
10-abhi <abhishek81936@gmail.com>
2025-09-03 23:59:01 +0530

    added video track rendering on receiver side

--- frontend/src/components/receiver.tsx
+++ frontend/src/components/receiver.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Receiver = () => {
+   const videoRef = useRef<HTMLVideoElement>(null);
+
    const [pc, setPc] = useState<RTCPeerConnection | null>(null);
    useEffect(() => {
       const socket = new WebSocket('ws://localhost:8080');
@@ -15,13 +17,20 @@ export const Receiver = () => {
             const newpc = new RTCPeerConnection();
             setPc(newpc);
             console.log("the peer conn", pc);
-            await pc?.setRemoteDescription(message.sdp);
+            await newpc?.setRemoteDescription(message.sdp);
             newpc.onicecandidate = (event) => {
                console.log("icecand event", event);
                if (event) {
                   socket.send(JSON.stringify({ type: 'iceCandidate', candidate: event.candidate }));
                }
             }
+
+            newpc.ontrack = (event)=>{
+               if(videoRef.current){
+                  videoRef.current.srcObject = new MediaStream([event.track]); 
+               }
+            }
+
             const answer = await pc?.createAnswer();
             await pc?.setLocalDescription(answer);
             socket.send(JSON.stringify({ type: "create-answer", sdp: answer }));
@@ -32,5 +41,6 @@ export const Receiver = () => {
    }, [])
    return <div>
       Receiver
+      <video ref={videoRef}></video>
    </div>
 }
