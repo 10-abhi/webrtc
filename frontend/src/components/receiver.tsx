c56da18886f61141fbc8a29913015148dd29b4cc
10-abhi <abhishek81936@gmail.com>
2025-09-05 02:28:07 +0530

    enhanced receiver side code and added tracks rendering

--- frontend/src/components/receiver.tsx
+++ frontend/src/components/receiver.tsx
@@ -1,46 +1,49 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect } from "react"
 
 export const Receiver = () => {
-   const videoRef = useRef<HTMLVideoElement>(null);
 
-   const [pc, setPc] = useState<RTCPeerConnection | null>(null);
    useEffect(() => {
       const socket = new WebSocket('ws://localhost:8080');
       socket.onopen = () => {
          socket.send(JSON.stringify({ type: "receiver" }));
+         startReceiving(socket);
       }
+   }, []);
+
+   function startReceiving(socket: WebSocket) {
+      const video = document.createElement('video');
+      document.body.appendChild(video);
+
+      const newpc = new RTCPeerConnection();
+      newpc.ontrack = (event)=>{
+         video.srcObject = new MediaStream([event.track]);
+         video.play();
+      }
+
+      if (!socket) return
       socket.onmessage = async (event) => {
          console.log("Received msg", event.data);
          const message = JSON.parse(event.data);
          if (message.type === "create-offer") {
             console.log("Matched create-offer");
-            const newpc = new RTCPeerConnection();
-            setPc(newpc);
-            console.log("the peer conn", pc);
             await newpc?.setRemoteDescription(message.sdp);
-            newpc.onicecandidate = (event) => {
-               console.log("icecand event", event);
-               if (event) {
-                  socket.send(JSON.stringify({ type: 'iceCandidate', candidate: event.candidate }));
-               }
-            }
-
-            newpc.ontrack = (event)=>{
-               if(videoRef.current){
-                  videoRef.current.srcObject = new MediaStream([event.track]); 
-               }
-            }
-
-            const answer = await pc?.createAnswer();
-            await pc?.setLocalDescription(answer);
+            const answer = await newpc?.createAnswer();
+            await newpc?.setLocalDescription(answer);
             socket.send(JSON.stringify({ type: "create-answer", sdp: answer }));
          } else if (message.type === "iceCandidate") {
-            await pc?.addIceCandidate(message.candidate);
+            await newpc?.addIceCandidate(message.candidate);
          }
       }
-   }, [])
-   return <div>
-      Receiver
-      <video ref={videoRef}></video>
-   </div>
+      newpc.onicecandidate = (event) => {
+         console.log("icecand event sending ice cand from receiver to server in frontend", event);
+         if (event.candidate) {
+            socket.send(JSON.stringify({ type: 'iceCandidate', candidate: event.candidate }));
+         }
+      }
+
+   }
+
+return <div>
+   Receiver
+</div>
 }
